74a166646e5d6b6b83e5d7ec9a9e601940e5d457
Enrique Lopez Jimenez <elopjim366@g.educaand.es>
2021-12-18 20:25:54 +0100

    Primer commit

---
+++ principal/Principal.java
@@ -0,0 +1,21 @@
+package principal;
+import java.util.Scanner;
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Programa que pregunta datos de alumnos de una clase (nombre y tres notas) y luego los lista sacando su nota media
+		
+				Scanner lector = new Scanner(System.in);  // la "herramienta lectora"
+				
+				String[] nombre; // el vector de nombres
+				double [][] nota; // la matriz de notas
+				
+				int nalum; // el número de alumnos que hay en la clase	
+				// variable de trabajo para el cálculo de las medias
+				double sumanotas=0; 
+				double media;
+
+	}
+
+}
