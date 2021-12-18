1a141c1a811262e44519210d11cfbf733b3c01d9
Enrique Lopez Jimenez <elopjim366@g.educaand.es>
2021-12-18 20:50:08 +0100

    Segundo commit

--- principal/Principal.java
+++ principal/Principal.java
@@ -15,6 +15,33 @@ public class Principal {
 				// variable de trabajo para el cálculo de las medias
 				double sumanotas=0; 
 				double media;
+				// PREGUNTAMOS EL nº DE ALUMNOS QUE HAY EN LA CLASE
+				
+				System.out.println("Introduzca el nº de alumnos que hay en esta clase: ");
+				nalum=lector.nextInt();
+				lector.nextLine();
+				
+				// Inicializamos los vectores de datos
+				
+				nombre= new String[nalum];
+				nota=new double[nalum][3];
+				
+				// AHORA EMPEZAMOS A PEDIR ALUMNOS (nombres y notas)
+				
+				for (int i=0 ; i<nalum;i++)
+				{
+					// el nombre
+					System.out.println("Introduzca el nombre del alumno nº "+i+1);
+					nombre[i]=lector.nextLine();
+					
+					// las notas
+					for (int j=0;j<=3;j++);
+					{
+						System.out.println("Introduzca la nota del alumno "+nombre[i]+" ");
+						lector.nextDouble();
+					}
+				}
+
 
 	}
 
